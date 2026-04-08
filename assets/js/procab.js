18d602684aadbaff834dd1ca0a98633c6fc7e2e8
Trương Minh Tiến <145457245+SyniaLouis@users.noreply.github.com>
2026-04-08 13:42:22 +0700

    Add saveFinalProgress function to track user progress

--- assets/js/procab.js
+++ assets/js/procab.js
@@ -5,14 +5,22 @@ export async function updateProcabScore(isCorrect, currentLessonId, wordIndex, c
     if (!currentUserData.procab_scores[currentLessonId]) {
         currentUserData.procab_scores[currentLessonId] = {};
     }
-    
     if (isCorrect) {
         currentUserData.procab_scores[currentLessonId][wordIndex] = 0;
     } else {
         let currentMistakes = currentUserData.procab_scores[currentLessonId][wordIndex] || 0;
         currentUserData.procab_scores[currentLessonId][wordIndex] = currentMistakes + 1;
     }
-    
+    const user = auth.currentUser;
+    if (user) {
+        await setDoc(doc(db, "users", user.uid), currentUserData, { merge: true });
+    }
+}
+
+export async function saveFinalProgress(currentLessonId, wordsCount, currentUserData, lessonsDb) {
+    let score = currentUserData.cumulative_progress[currentLessonId] || 0;
+    const target = lessonsDb[currentLessonId].num * 5;
+    currentUserData.cumulative_progress[currentLessonId] = Math.min(score + wordsCount, target);
     const user = auth.currentUser;
     if (user) {
         await setDoc(doc(db, "users", user.uid), currentUserData, { merge: true });
